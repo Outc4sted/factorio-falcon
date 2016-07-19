a47e19e0e91a8a9279bd5e69d57212ad676bd28c
Justen Falk <Outc4sted@gmail.com>
2016-07-19 18:11:54 -0400

    Copied Raven files and altered some metadata for the Falcon

---
+++ control.lua
@@ -0,0 +1,116 @@
+global.flycar = {{},{}}
+
+local function build (ent)
+	if ent.name =="falcon-1" then
+		table.insert(global.flycar[1], ent)
+	end
+end
+
+local function car_flying ()
+	local cars = global.flycar
+	for i,j in pairs(cars[1]) do
+		if j.valid then
+			if j.speed >= 0.3 then
+				local fly = {position = j.position, speed = j.speed, orientation = j.orientation, health = j.health}
+				local idx = j.passenger
+				local surface = j.surface
+				local inv = j.get_inventory(1).get_contents()
+				local inv2 = j.get_inventory(2).get_contents()
+				j.destroy()
+				j = surface.create_entity{name = "falcon-2", position = fly.position, force =  game.forces.player}
+				for y,u in pairs(inv) do
+					j.insert({name = y, count = u})
+				end
+				for y,u in pairs(inv2) do
+					j.insert({name = y, count = u})
+				end
+				j.orientation = fly.orientation
+				j.speed = fly.speed
+				j.health = fly.health
+				j.passenger = idx
+				local shadow = surface.create_entity{name = "falcon-shadow", position = fly.position, force =  game.forces.neutral}
+				shadow.insert({name = "solid-fuel", count = 50})
+				shadow.orientation = fly.orientation
+				shadow.speed = fly.speed
+				table.insert(cars[2], {j,shadow,0.1})
+			end
+		else table.remove(cars[1], i)
+		end
+	end
+	for i,j in pairs(cars[2]) do
+		if j[1].valid then
+			if j[1].speed <= 0.29 then
+				local idx = j[1].passenger
+				local surface = j[1].surface
+				local tilename = surface.get_tile(math.floor(j[1].position.x), math.floor(j[1].position.y)).name
+				if tilename == "deepwater" or tilename == "water" then
+					j[1].die()
+					j[2].destroy()
+					if idx ~= nil then
+						if idx.valid then
+							local position = idx.position
+							tilename = surface.get_tile(math.floor(position.x), math.floor(position.y)).name
+							if tilename == "deepwater" or tilename == "water" then idx.die() return
+							else idx.health = idx.health - math.random(1,52)
+								if idx.health == 0 then idx.die() return end
+							end
+						end
+					end
+				else
+					local fly = {position = j[1].position, speed = j[1].speed, orientation = j[1].orientation, health = j[1].health}
+					local inv = j[1].get_inventory(1).get_contents()
+					local inv2 = j[1].get_inventory(2).get_contents()
+					j[1].destroy()
+					j[2].destroy()
+					j = surface.create_entity{name = "falcon-1", position = fly.position, force =  game.forces.player}
+					for y,u in pairs(inv) do
+						j.insert({name = y, count = u})
+					end
+					for y,u in pairs(inv2) do
+						j.insert({name = y, count = u})
+					end
+					j.orientation = fly.orientation
+					j.speed = fly.speed
+					j.health = fly.health
+					if tilename == "sand" or tilename == "sand-dark" then j.health = j.health - math.random(1,12) end
+					j.passenger = idx
+					table.insert(cars[1], j)
+				end
+			else
+				local chight = (j[1].speed - 0.29)*10 - j[3]
+				j[3] = j[3] + chight
+				j[1].teleport({j[1].position.x, j[1].position.y - chight})
+				j[2].teleport({j[1].position.x + j[3], j[1].position.y + j[3]})
+				j[2].orientation = j[1].orientation
+				j[2].speed = j[1].speed
+			end
+		else
+			if j[2].valid then j[2].destroy() end
+			table.remove(cars[2], i)
+		end
+	end
+end
+
+local function drive_control(idx)
+	local player = game.players[idx]
+	if not player.driving then
+		local tilename = player.surface.get_tile(math.floor(player.position.x), math.floor(player.position.y)).name
+		if tilename == "deepwater" or tilename == "water" then player.character.die() return end
+	end
+end
+
+script.on_event(defines.events.on_player_driving_changed_state, function(event)
+	drive_control(event.player_index)
+end)
+
+script.on_event(defines.events.on_built_entity, function(event)
+	build(event.created_entity)
+end)
+
+script.on_event(defines.events.on_robot_built_entity, function(event)
+	build(event.created_entity)
+end)
+
+script.on_event(defines.events.on_tick, function()
+	car_flying()
+end)
---
+++ data.lua
@@ -0,0 +1,578 @@
+data:extend({
+  {
+    type = "item",
+    name = "falcon-1",
+    icon = "__Raven__/raven/Raven_icon.png",
+    flags = {"goes-to-quickbar"},
+    subgroup = "transport",
+    order = "b[personal-transport]-a[car]",
+    place_result = "falcon-1",
+    stack_size = 50
+  },
+  {
+    type = "car",
+    name = "falcon-1",
+    icon = "__Raven__/raven/Raven_icon.png",
+    flags = {"pushable", "placeable-neutral", "player-creation", "placeable-off-grid"},
+    minable = {mining_time = 1, result = "falcon-1"},
+    max_health = 200,
+    corpse = "medium-remnants",
+    dying_explosion = "medium-explosion",
+    energy_per_hit_point = 1,
+    crash_trigger = crash_trigger(),
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 10
+      },
+      {
+        type = "impact",
+        percent = 10,
+        decrease = 1
+      }
+    },
+    collision_box = {{-0.7, -1}, {0.7, 1}},
+    selection_box = {{-0.7, -1}, {0.7, 1}},
+    effectivity = 0.17,
+    braking_power = "20kW",
+    burner =
+    {
+      effectivity = 0.3,
+      fuel_inventory_size = 1,
+      smoke =
+      {
+        {
+          name = "car-smoke",
+          deviation = {0.25, 0.25},
+          frequency = 200,
+          position = {0, 1.5},
+          starting_frame = 0,
+          starting_frame_deviation = 60
+        }
+      }
+    },
+    consumption = "200kW",
+    friction = 2e-3,
+    light =
+    {
+      {
+        minimum_darkness = 0.3,
+        intensity = 0.4,
+        size = 10,
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-0.7, -13},
+        size = 2,
+        intensity = 1.6
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {0.4, -13},
+        size = 2,
+        intensity = 1.6
+      }
+    },
+    animation =
+    {
+      layers =
+      {
+        {
+          width = 128,
+          height = 128,
+          frame_count = 2,
+          direction_count = 72,
+          shift = {0, 0},
+          animation_speed = 1,
+          max_advance = 0.2,
+          stripes =
+          {
+            {
+             filename = "__Raven__/raven/Raven_01.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_01_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_2.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+          }
+        },
+        {
+          width = 128,
+          height = 128,
+          frame_count = 2,
+          draw_as_shadow = true,
+          direction_count = 72,
+          shift = {0.28125, 0.25},
+          max_advance = 0.2,
+          stripes = util.multiplystripes(2,
+          {
+            {
+             filename = "__Raven__/raven/Raven_01_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+          })
+        }
+      }
+    },
+    sound_no_fuel =
+    {
+      {
+        filename = "__base__/sound/fight/car-no-fuel-1.ogg",
+        volume = 0.6
+      },
+    },
+    sound_minimum_speed = 0.2;
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/car-engine.ogg",
+        volume = 0.6
+      },
+      --[[activate_sound =
+      {
+        filename = "__base__/sound/car-engine-start.ogg",
+        volume = 0.6
+      },]]
+      deactivate_sound =
+      {
+        filename = "__base__/sound/car-engine-stop.ogg",
+        volume = 0.6
+      },
+      match_speed_to_activity = true,
+    },
+    open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
+    close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
+    rotation_speed = 0.003,
+    weight = 500,
+    inventory_size = 50
+  },
+  {
+    type = "car",
+    name = "falcon-2",
+    icon = "__Raven__/raven/Raven_icon.png",
+    subgroup = "transport",
+    order = "b[personal-transport]-a[car]",
+    flags = {"pushable", "placeable-neutral", "player-creation", "placeable-off-grid"},
+    max_health = 200,
+    corpse = "medium-remnants",
+    dying_explosion = "medium-explosion",
+    energy_per_hit_point = 1,
+    crash_trigger = crash_trigger(),
+    resistances =
+    {
+      {
+        type = "fire",
+        percent = 10
+      },
+      {
+        type = "impact",
+        percent = 10,
+        decrease = 1
+      }
+    },
+    collision_box = {{-0.7, -1}, {0.7, 1}},
+    selection_box = {{-0.7, -1}, {0.7, 1}},
+  collision_mask = {"not-colliding-with-itself"},
+    effectivity = 0.3,
+    braking_power = "20kW",
+    burner =
+    {
+      effectivity = 0.3,
+      fuel_inventory_size = 1,
+      smoke =
+      {
+        {
+          name = "car-smoke",
+          deviation = {0.25, 0.25},
+          frequency = 200,
+          position = {0, 1.5},
+          starting_frame = 0,
+          starting_frame_deviation = 60
+        }
+      }
+    },
+    consumption = "200kW",
+    friction = 1e-3,
+    light =
+    {
+      {
+        minimum_darkness = 0.3,
+        intensity = 0.4,
+        size = 10,
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {-0.7, -13},
+        size = 2,
+        intensity = 0.6
+      },
+      {
+        type = "oriented",
+        minimum_darkness = 0.3,
+        picture =
+        {
+          filename = "__core__/graphics/light-cone.png",
+          priority = "medium",
+          scale = 2,
+          width = 200,
+          height = 200
+        },
+        shift = {0.4, -13},
+        size = 2,
+        intensity = 0.6
+      }
+    },
+    animation =
+    {
+      layers =
+      {
+        {
+          width = 128,
+          height = 128,
+          frame_count = 4,
+          direction_count = 72,
+          shift = {0, 0},
+          animation_speed = 1,
+          max_advance = 0.2,
+          stripes =
+          {
+            {
+             filename = "__Raven__/raven/Raven_01.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_01_1.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_01_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_01_3.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_1.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_3.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_1.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_3.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_1.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_2.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_3.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_1.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_2.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_3.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+          }
+        }
+      }
+    },
+    sound_no_fuel =
+    {
+      {
+        filename = "__base__/sound/fight/car-no-fuel-1.ogg",
+        volume = 0.6
+      },
+    },
+    sound_minimum_speed = 0.2;
+    vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
+    working_sound =
+    {
+      sound =
+      {
+        filename = "__base__/sound/car-engine.ogg",
+        volume = 0.6
+      },
+      --[[activate_sound =
+      {
+        filename = "__base__/sound/car-engine-start.ogg",
+        volume = 0.6
+      },]]
+      deactivate_sound =
+      {
+        filename = "__base__/sound/car-engine-stop.ogg",
+        volume = 0.6
+      },
+      match_speed_to_activity = true,
+    },
+    open_sound = { filename = "__base__/sound/car-door-open.ogg", volume=0.7 },
+    close_sound = { filename = "__base__/sound/car-door-close.ogg", volume = 0.7 },
+    rotation_speed = 0.006,
+    weight = 500,
+    inventory_size = 50
+  },
+  {
+    type = "car",
+    name = "falcon-shadow",
+    icon = "__Raven__/raven/Raven_icon.png",
+    subgroup = "transport",
+    order = "b[personal-transport]-a[car]",
+    flags = {"not-on-map", "placeable-off-grid"},
+    max_health = 0,
+    energy_per_hit_point = 1,
+    collision_box = {{-0.7, -1}, {0.7, 1}},
+    selection_box = {{0, 0}, {0, 0}},
+  collision_mask = {"not-colliding-with-itself"},
+    effectivity = 1,
+    braking_power = "0kW",
+    burner =
+    {
+      effectivity = 1,
+      fuel_inventory_size = 1,
+    },
+    consumption = "0kW",
+    friction = 2e-3,
+    animation =
+    {
+      layers =
+      {
+        {
+          width = 128,
+          height = 128,
+          frame_count = 2,
+          draw_as_shadow = true,
+          direction_count = 72,
+          shift = {0.28125, 0.25},
+          max_advance = 0.2,
+          stripes = util.multiplystripes(2,
+          {
+            {
+             filename = "__Raven__/raven/Raven_01_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_02_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_03_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_04_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 16,
+            },
+            {
+             filename = "__Raven__/raven/Raven_05_Shadow.png",
+             width_in_frames = 1,
+             height_in_frames = 8,
+            },
+          })
+        }
+      }
+    },
+    rotation_speed = 0,
+    inventory_size = 0
+  },
+  {
+    type = "recipe",
+    name = "raven",
+    energy_required = 30,
+    enabled = false,
+    category = "crafting",
+    ingredients =
+    {
+      {"plastic-bar", 100},
+      {"advanced-circuit", 200},
+      {"electric-engine-unit", 20},
+      {"battery", 100}
+    },
+    result = "falcon-1"
+  },
+  {
+    type = "technology",
+    name = "raven",
+    icon = "__Raven__/raven/Raven_icon_64.png",
+  icon_size = 64,
+    effects =
+    {
+      {
+        type = "unlock-recipe",
+        recipe = "raven"
+      }
+    },
+    prerequisites = {"flying"},
+    unit =
+    {
+      count = 50,
+      ingredients =
+      {
+        {"science-pack-1", 3},
+        {"science-pack-2", 2}
+      },
+      time = 30
+    },
+    order = "c-k",
+  }
+})
